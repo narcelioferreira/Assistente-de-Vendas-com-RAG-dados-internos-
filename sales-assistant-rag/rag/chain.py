7275e1c6dbb478bb314dc4b599ed553ec27d5a62
Francisco Narcelio <narcelio.ferreirapsi@gmail.com>
2026-05-06 09:47:17 -0300

    Correção

--- sales-assistant-rag/rag/chain.py
+++ sales-assistant-rag/rag/chain.py
@@ -4,10 +4,10 @@ Módulo da chain RAG: conecta retriever + LLM para gerar respostas contextualiza
 import os
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
 
 from rag.ingestor import load_all_documents
 from rag.vector_store import build_vector_store, get_retriever
